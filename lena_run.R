fcaa02df97efeda2eb548e445cb4ff5d0b4524d3
Simon Wolanin <sw@awp.ch>
2020-08-29 14:21:35 +0200

    Update 29.08.

--- lena_run.R
+++ lena_run.R
@@ -61,15 +61,15 @@ results <- merge(results,Ja_Stimmen_Kanton)
 
 
 #Kanton Jura entfernen
-for (c in 1:nrow(results)) {
+#for (c in 1:nrow(results)) {
   
-  if (results$Kantons_Nr[c] == 26) {
+#  if (results$Kantons_Nr[c] == 26) {
 
-results$Gebiet_Ausgezaehlt[c] <- FALSE
+#results$Gebiet_Ausgezaehlt[c] <- FALSE
 
-}
+#}
 
-}
+#}
 
 
 #Wie viele Gemeinden sind ausgezählt?
